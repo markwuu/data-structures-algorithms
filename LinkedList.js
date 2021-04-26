a6588f113d12449fa0c3c304741a79d5b01de875
Mark W <markwuu@gmail.com>
2021-04-25 21:46:21 -0700

    Created insert last data node to linked list function

--- LinkedList.js
+++ LinkedList.js
@@ -18,10 +18,39 @@ class LinkedList {
     insertFirst(data) {
         this.head = new Node(data, this.head);
         this.size++;
-        return;
     }
 
     // Insert last node
+    // Create a node
+    // Create a current variable
+    // If ll is empty, make the data the head
+    // Otherwise set current to the head
+    // Traverse down the linked list
+    // While there is a current.next data point, then move the current
+    // variable over one, so we can land on the last tail
+    // 1 (curr) => 2 => 3 => null // while(current.next) // while(2)
+    // 1 => 2 (curr) => 3 => null // while(current.next) // while(3)
+    // 1 => 2 => 3 (curr) => null // while(current.next) // while(null)
+    // While loop breaks with current on the last node
+    // Set the last node's next to the data node
+    insertLast(data) {
+        let node = new Node(data);
+        let current;
+
+        //If linked list is empty, make the data the head
+        if(!this.head){
+            this.head = node;
+        } else {
+            current = this.head;
+
+            while(current.next){
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+        this.size++;
+    }
 
     // Insert at index
 
@@ -33,7 +62,7 @@ class LinkedList {
 
     // Print list data
     // 1 => 2 => 3 => null
-    // set current to the head;
+    // set current to the head
     // while current has a value
     // print out that data
     // then set current to the next data value
@@ -52,6 +81,10 @@ class LinkedList {
 
 const ll = new LinkedList();
 
-ll.insertFirst(100);
+ll.insertFirst(300);
 ll.insertFirst(200);
+ll.insertFirst(100);
+ll.insertLast(400);
+ll.insertLast(500);
+ll.insertLast(600);
 ll.printList();
