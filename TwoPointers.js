c97fe0155a76865325128965f8421bf2afe8edf8
Mark W <markwuu@gmail.com>
2021-08-15 11:25:30 -0700

    Implemented find length of array after duplicate removal

--- TwoPointers.js
+++ TwoPointers.js
@@ -19,5 +19,33 @@ const find_pair_target_sum = (array, target) => {
     return [-1, -1];
 }
 
-console.log(find_pair_target_sum([1, 2, 3, 4, 6], 6)); //[1, 3]
-console.log(find_pair_target_sum([2, 5, 9, 11], 11)); //[0, 2]
+// console.log(find_pair_target_sum([1, 2, 3, 4, 6], 6)); //[1, 3]
+// console.log(find_pair_target_sum([2, 5, 9, 11], 11)); //[0, 2]
+
+const find_length_after_duplicate_removal = (arr) => {
+    let left = 0;
+    let right = arr.length - 1;
+    let integerHashMap = {};
+
+    while(left < right) {
+        let leftChar = arr[left];
+        let rightChar = arr[right];
+
+        if(!(leftChar in integerHashMap)) {
+            integerHashMap[leftChar] = 0;
+        }
+        if(!(rightChar in integerHashMap)) {
+            integerHashMap[rightChar] = 0;
+        }
+
+        integerHashMap[leftChar] = 1;
+        integerHashMap[rightChar] = 1;
+
+        left += 1;
+        right -=1;
+    }
+
+    return Object.keys(integerHashMap).length;
+}
+
+console.log(find_length_after_duplicate_removal([2, 3, 3, 3, 6, 9, 9])) //4
